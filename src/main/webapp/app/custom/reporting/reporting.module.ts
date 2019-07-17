b5d81e8236a094dc4067f29094b916393a277878
Edwin Njeru <mailnjeru@gmail.com>
2019-07-17 09:03:57 +0300

    Scaffold reporting module

--- src/main/webapp/app/custom/reporting/reporting.module.ts
+++ src/main/webapp/app/custom/reporting/reporting.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+/**
+ * This module features reports which are generated in the back-end and then sent to
+ * the front-end as complete reports in their respective media, which could be excel,
+ * pdf, html...
+ */
 @NgModule({
   declarations: [],
   imports: [CommonModule]
