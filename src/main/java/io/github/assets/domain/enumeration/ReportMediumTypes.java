29ecd02e3a268be8624100df17e9a8540c8d8f64
Edwin Njeru <mailnjeru@gmail.com>
2019-07-17 09:57:11 +0300

    Add important enumerations

---
+++ src/main/java/io/github/assets/domain/enumeration/ReportMediumTypes.java
@@ -0,0 +1,8 @@
+package io.github.assets.domain.enumeration;
+
+/**
+ * The ReportMediumTypes enumeration.
+ */
+public enum ReportMediumTypes {
+    EXCEL, PDF, POWERPOINT, DOC, TEXT, JSON, HTML5
+}
---
+++ src/main/java/io/github/assets/domain/enumeration/TitleTypes.java
@@ -0,0 +1,8 @@
+package io.github.assets.domain.enumeration;
+
+/**
+ * Provide the titles you can have for a purveyor or dealer
+ */
+public enum TitleTypes {
+    DR, ENG, HON, JUST, MES, MISS, MMS, MR, MRS, MS, MST, PROF, REV, CPA
+}
