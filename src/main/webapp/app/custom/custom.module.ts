c945742bae3b6206b5f796ebd85cc07fbf220cfc
Edwin Njeru <mailnjeru@gmail.com>
2019-07-17 08:56:09 +0300

    Implement data-export module for exporting data into various formats

--- src/main/webapp/app/custom/custom.module.ts
+++ src/main/webapp/app/custom/custom.module.ts
@@ -3,23 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { CustomRoutingModule } from './custom-routing.module';
 import { AboutFixedAssetsModule } from 'app/custom/about-fixed-assets';
-import {AboutFixedAssetsRoutingModule} from 'app/custom/about-fixed-assets/about-fixed-assets-routing.module';
+import { AboutFixedAssetsRoutingModule } from 'app/custom/about-fixed-assets/about-fixed-assets-routing.module';
 import { NavigationModule } from './navigation/navigation.module';
 import { QuestionnaireModule } from './questionnaire/questionnaire.module';
+import { DataExportModule } from './data-export/data-export.module';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    CustomRoutingModule,
-    AboutFixedAssetsModule,
-    NavigationModule,
-    QuestionnaireModule
-  ],
-  exports: [
-    AboutFixedAssetsModule,
-    AboutFixedAssetsRoutingModule,
-    NavigationModule
-  ]
+  imports: [CommonModule, CustomRoutingModule, AboutFixedAssetsModule, NavigationModule, QuestionnaireModule, DataExportModule],
+  exports: [AboutFixedAssetsModule, AboutFixedAssetsRoutingModule, NavigationModule, DataExportModule]
 })
-export class CustomModule { }
+export class CustomModule {}
---
+++ src/main/webapp/app/custom/data-export/data-export.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DataExportRoutingModule } from './data-export-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, DataExportRoutingModule]
+})
+export class DataExportModule {}
