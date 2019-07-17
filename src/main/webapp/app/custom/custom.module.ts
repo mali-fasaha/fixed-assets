b9e64db3289e04afb20bf90e8a6928438ae545b8
Edwin Njeru <mailnjeru@gmail.com>
2019-07-17 07:58:30 +0300

    Add navigation module

--- src/main/webapp/app/custom/custom.module.ts
+++ src/main/webapp/app/custom/custom.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { CustomRoutingModule } from './custom-routing.module';
 import { AboutFixedAssetsModule } from 'app/custom/about-fixed-assets';
 import {AboutFixedAssetsRoutingModule} from 'app/custom/about-fixed-assets/about-fixed-assets-routing.module';
+import { NavigationModule } from './navigation/navigation.module';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     CustomRoutingModule,
-    AboutFixedAssetsModule
+    AboutFixedAssetsModule,
+    NavigationModule
   ],
   exports: [
     AboutFixedAssetsModule, AboutFixedAssetsRoutingModule
