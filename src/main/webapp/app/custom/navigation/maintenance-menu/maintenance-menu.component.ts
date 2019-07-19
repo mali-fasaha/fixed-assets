583e586a063253c5bf31c3438e6184600d36a24a
Edwin Njeru <mailnjeru@gmail.com>
2019-07-19 14:02:30 +0300

    Scaffold maintenance Menu

---
+++ src/main/webapp/app/custom/navigation/maintenance-menu/maintenance-menu.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'gha-maintenance-menu',
+  templateUrl: './maintenance-menu.component.html',
+  styleUrls: ['./maintenance-menu.component.scss']
+})
+export class MaintenanceMenuComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
--- src/main/webapp/app/custom/navigation/navigation.module.ts
+++ src/main/webapp/app/custom/navigation/navigation.module.ts
@@ -8,12 +8,34 @@ import { ReportMenuComponent } from './report-menu/report-menu.component';
 import { ImportMenuComponent } from './import-menu/import-menu.component';
 import { ExportMenuComponent } from './export-menu/export-menu.component';
 import { FixedAssetsSharedModule } from 'app/shared';
+import { MaintenanceMenuComponent } from './maintenance-menu/maintenance-menu.component';
 
 @NgModule({
-  declarations: [AboutMenuComponent, ModelMenuComponent, ReportMenuComponent, ImportMenuComponent, ExportMenuComponent],
+  declarations: [
+    AboutMenuComponent,
+    ModelMenuComponent,
+    ReportMenuComponent,
+    ImportMenuComponent,
+    ExportMenuComponent,
+    MaintenanceMenuComponent
+  ],
   imports: [CommonModule, NavigationRoutingModule, FixedAssetsSharedModule],
-  exports: [AboutMenuComponent, ModelMenuComponent, ReportMenuComponent, ImportMenuComponent, ExportMenuComponent],
-  entryComponents: [AboutMenuComponent, ModelMenuComponent, ReportMenuComponent, ImportMenuComponent, ExportMenuComponent],
+  exports: [
+    AboutMenuComponent,
+    ModelMenuComponent,
+    ReportMenuComponent,
+    ImportMenuComponent,
+    ExportMenuComponent,
+    MaintenanceMenuComponent
+  ],
+  entryComponents: [
+    AboutMenuComponent,
+    ModelMenuComponent,
+    ReportMenuComponent,
+    ImportMenuComponent,
+    ExportMenuComponent,
+    MaintenanceMenuComponent
+  ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class NavigationModule {}
