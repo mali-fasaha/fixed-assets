25e0ccb048c6f121d019d2c0c83d96f87f04d71b
Edwin Njeru <mailnjeru@gmail.com>
2019-07-19 14:20:13 +0300

    Scaffold data-tables menu

---
+++ src/main/webapp/app/custom/navigation/data-tables-menu/data-tables-menu.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'gha-data-tables-menu',
+  templateUrl: './data-tables-menu.component.html',
+  styleUrls: ['./data-tables-menu.component.scss']
+})
+export class DataTablesMenuComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
--- src/main/webapp/app/custom/navigation/navigation.module.ts
+++ src/main/webapp/app/custom/navigation/navigation.module.ts
@@ -8,12 +8,34 @@ import { ReportMenuComponent } from './report-menu/report-menu.component';
 import { ImportMenuComponent } from './import-menu/import-menu.component';
 import { FixedAssetsSharedModule } from 'app/shared';
 import { MaintenanceMenuComponent } from './maintenance-menu/maintenance-menu.component';
+import { DataTablesMenuComponent } from './data-tables-menu/data-tables-menu.component';
 
 @NgModule({
-  declarations: [AboutMenuComponent, ModelMenuComponent, ReportMenuComponent, ImportMenuComponent, MaintenanceMenuComponent],
+  declarations: [
+    AboutMenuComponent,
+    ModelMenuComponent,
+    ReportMenuComponent,
+    ImportMenuComponent,
+    MaintenanceMenuComponent,
+    DataTablesMenuComponent
+  ],
   imports: [CommonModule, NavigationRoutingModule, FixedAssetsSharedModule],
-  exports: [AboutMenuComponent, ModelMenuComponent, ReportMenuComponent, ImportMenuComponent, MaintenanceMenuComponent],
-  entryComponents: [AboutMenuComponent, ModelMenuComponent, ReportMenuComponent, ImportMenuComponent, MaintenanceMenuComponent],
+  exports: [
+    AboutMenuComponent,
+    ModelMenuComponent,
+    ReportMenuComponent,
+    ImportMenuComponent,
+    MaintenanceMenuComponent,
+    DataTablesMenuComponent
+  ],
+  entryComponents: [
+    AboutMenuComponent,
+    ModelMenuComponent,
+    ReportMenuComponent,
+    ImportMenuComponent,
+    MaintenanceMenuComponent,
+    DataTablesMenuComponent
+  ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class NavigationModule {}
