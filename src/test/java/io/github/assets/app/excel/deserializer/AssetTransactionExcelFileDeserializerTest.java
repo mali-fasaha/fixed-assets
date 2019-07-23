96160c28d1ecbf9fd0a8378afe35b37b3bd423e6
Edwin Njeru <mailnjeru@gmail.com>
2019-07-23 14:14:30 +0300

    Complete asset-trasnsaction-excel-file-deserializer-test

--- src/test/java/io/github/assets/app/excel/deserializer/AssetTransactionExcelFileDeserializerTest.java
+++ src/test/java/io/github/assets/app/excel/deserializer/AssetTransactionExcelFileDeserializerTest.java
@@ -1,18 +1,16 @@
 package io.github.assets.app.excel.deserializer;
 
 import io.github.assets.app.model.AssetTransactionEVM;
-import org.joda.time.LocalDate;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.util.Date;
 import java.util.List;
 
 import static io.github.assets.app.excel.ExcelTestUtil.getDefaultPoijiOptions;
 import static io.github.assets.app.excel.ExcelTestUtil.readFile;
 import static io.github.assets.app.excel.ExcelTestUtil.toBytes;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class AssetTransactionExcelFileDeserializerTest {
 
@@ -21,8 +19,7 @@ public class AssetTransactionExcelFileDeserializerTest {
     @Before
     public void setUp() throws Exception {
 
-        assetTransactionExcelFileDeserializer =
-            new AssetTransactionExcelFileDeserializer(getDefaultPoijiOptions());
+        assetTransactionExcelFileDeserializer = new AssetTransactionExcelFileDeserializer(getDefaultPoijiOptions());
     }
 
     @Test
@@ -35,7 +32,7 @@ public class AssetTransactionExcelFileDeserializerTest {
         // @formatter:on
 
         assertEquals(1000, transactions.size());
-        assertEquals("Wrong transaction id! : ","TRN 218", transactions.get(0).getTransactionReference());
+        assertEquals("Wrong transaction id! : ", "TRN 218", transactions.get(0).getTransactionReference());
         assertEquals("Wrong Transaction date! : ", "2018/08/16", transactions.get(0).getTransactionDate());
         assertEquals("Wrong Scanned document! : ", 555, transactions.get(0).getScannedDocumentId());
     }
