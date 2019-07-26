61ab9d412ce346b4f444b411b8510a6ee1a3fbf5
Edwin Njeru <mailnjeru@gmail.com>
2019-07-26 19:04:53 +0300

    Add side-nav component

---
+++ src/main/webapp/app/custom/side-navigation/side-nav.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'gha-side-nav',
+  templateUrl: './side-nav.component.html',
+  styleUrls: ['./side-nav.component.scss']
+})
+export class SideNavComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
--- src/main/webapp/app/custom/side-navigation/side-navigation.module.ts
+++ src/main/webapp/app/custom/side-navigation/side-navigation.module.ts
@@ -2,10 +2,13 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SideNavigationRoutingModule } from './side-navigation-routing.module';
+import { SideNavComponent } from './side-nav.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [SideNavComponent],
   imports: [CommonModule, SideNavigationRoutingModule],
+  exports: [SideNavComponent],
+  entryComponents: [SideNavComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class SideNavigationModule {}
