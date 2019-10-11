ae964a23bde6ee4de547986279d7ec608e4022e8
Edwin Njeru <mailnjeru@gmail.com>
2019-10-11 16:51:54 +0300

    Create file model type enumeration

---
+++ src/main/java/io/github/assets/domain/enumeration/FileModelType.java
@@ -0,0 +1,5 @@
+package io.github.assets.domain.enumeration;
+
+public enum FileModelType {
+    DEPRECIATION_UPLOAD, ACQUISITION_UPLOAD, DISPOSAL_UPLOAD, CWIP_UPLOAD, CWIP_TRANSFER_UPLOAD, ASSET_ITEM_UPLOAD
+}
